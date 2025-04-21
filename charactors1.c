f0b96b3e082a2895726af29f2b2253586de2383e
Pragati Gupta <praeast94@gmail.com>
2025-04-22 00:01:46 +0530

    input problem related to char is solved in charactors1.c

--- charactors1.c
+++ charactors1.c
@@ -29,7 +29,7 @@ void middle(char x, char y, char z){
 		printf("middle char among %c, %c, %c = %c\n\n", x, y, z, x);
 	}
 	else if((y > x && y < z) || (y > z && y < x)){
-		printf("middle char among %c, %c, %c = %c\n ", x, y, z, y);
+		printf("middle char among %c, %c, %c = %c\n", x, y, z, y);
 	}
 	else{
 		printf("middle char among %c, %c, %c = %c\n\n", x, y, z, z);
@@ -63,30 +63,30 @@ void compute(int a, int b, char c){
 void main(){
 	char ch, x, y, z;
 	int a, b, c, p;
-	printf("enter 1  to change case of a charactor\nenter 2  to know whether a charactor is a digit\nenter 3  to know middle charactor among three charactors\nenter 4  to compute two numbers with a charactor\nenter -1  to exit\n");
+	printf("enter 1  to change case of a charactor\nenter 2  to know whether a charactor is a digit\nenter 3  to know middle charactor among three charactors\nenter 4  to compute two numbers with a charactor\nenter 0  to exit\n");
 	scanf("%d", &p);
-	for( ; p != -1; ){
+	for(;p != 0 ;){
 		if( p == 1 ){
 			printf("give a charactor = ");
-			scanf("%c",&ch);
+			scanf(" %c", &ch);
 			changeCase(ch);
 		}
 		if( p == 2 ){
 			printf("give a charactor = ");
-			scanf("%c", &ch);
+			scanf(" %c", &ch);
 			isDigit(ch);
 		}
 		if( p == 3 ){
-			printf("give 3 charactors = ");
-			scanf("%c %c %c", &x, &y, &z);
+		       	printf("give 3 charactors = ");
+			scanf(" %c %c %c", &x, &y, &z);
 			middle(x, y, z);
 		}
 		if( p == 4 ){
-			printf("given two numbers and a operator =");
+		       	printf("given two numbers and a operator =");
 	 		scanf("%d %d %c", &a, &b, &c);
 			compute(a, b, c);
 		}
-	printf("enter 1  to change case of a charactor\nenter 2  to know whether a charactor is a digit\nenter 3  to know middle charactor among three charactors\nenter 4  to compute two numbers with a charactor\nenter -1  to exit\n");
+	printf("enter 1  to change case of a charactor\nenter 2  to know whether a charactor is a digit\nenter 3  to know middle charactor among three charactors\nenter 4  to compute two numbers with a charactor\nenter 0  to exit\n");
 	scanf("%d", &p);
 	}
 }
