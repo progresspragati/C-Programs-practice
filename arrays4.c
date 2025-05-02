64b5579cffee6c9c1b5d9b5148dd9a8854f10133
Pragati Gupta <praeast94@gmail.com>
2025-05-02 16:15:06 +0530

    correction in secondLargest function of arrays4.c because of wrond result by giving input{1,4,3,2}

--- arrays4.c
+++ arrays4.c
@@ -10,26 +10,27 @@ void thirdLargest(int a[], int x){
 			l2 = l1;
 			l1 = a[i];
 		}
-		if(l1 == l2){
-			if(l1 == a[i] && counter < 3){
-				counter++;
-			}
-			if(counter >= 2){
-				l2 = l1;
-			}
-			if(counter == 3){
-				l3 = l1;
-			}
-			else{
-				if(l2 < a[i] && a[i] != l1){
-					l3 = l2;
-					l2 = a[i];
-				}
-				if((l3 < a[i]) && (a[i] != l1) && (a[i] != l2)){
-					l3 = a[i];
-				}
-			}
+		if(l1 == a[0]){
+			counter++;
 		}
+		if(l1 == a[i] && counter < 3){
+			counter++;
+		}
+		if(counter >= 2){
+			l2 = l1;
+		}
+		if(counter == 3){
+			l3 = l1;
+		}
+		else{
+			if(l2 < a[i] && a[i] != l1){
+				l3 = l2;
+				l2 = a[i];
+			}
+			if((l3 < a[i]) && (a[i] != l1) && (a[i] != l2)){
+				l3 = a[i];
+			}
+		}		
 	}
 	printf("%d, %d, %d", l1, l2, l3);
 }
@@ -39,12 +40,12 @@ void secondLargest(int a[], int x){
 	int l2 = a[0];
 	int counter = 0;
 	for(int i = 1; i < x; i++){
-		if(l1 < a[i]){
+		if(l1 <= a[i]){
 			l2 = l1;
 			l1 = a[i];
 		}
 	}
-	if(l1 == l2){
+	//if(l1 == l2){
 		for(int i = 0; i < x && counter < 2; i++){
 			if(l1 == a[i]){
 				counter++;
@@ -63,12 +64,50 @@ void secondLargest(int a[], int x){
 				}
 			}
 		}
-	}
+	//}
 	printf("%d, %d", l1, l2);
 }
 
+
+void thirdLargestOfArray(int a[], int x){
+	int l1 = a[0];
+	int l2 = a[0];
+	int l3 = a[0];
+	int counter = 0;
+	for(int i = 1; i < x; i++){
+		if(l1 <= a[i]){
+			l3 = l2;
+			l2 = l1;
+			l1 = a[i];
+		}
+	}
+	for(int i = 0; i < x && counter < 3; i++){
+		if(l1 == a[i]){
+			counter++;
+		}
+	}
+	if(counter >= 2){
+		l2 = l1;
+	}
+	if(counter == 3){
+		l3 = l1;
+	}
+	else{
+		for(int i = 0; i < x; i++){
+			if((a[i] != l1) && (l2 == l1)){
+				l3 = l2;
+				l2 = a[i];
+			}
+			if(l2 < a[i] && a[i] != l1){
+				l2 = a[i];
+			}
+		}
+	}
+	printf("%d, %d", l1, l2);
+}	
 void main(){
-	int a[4] = {1, 2, 4, 4};
-	thirdLargest(a,4);
-	// secondLargest(a, 4);
+	int a[4] = {1, 4, 3, 2};
+	//thirdLargest(a,4);
+	//thirdLargestOfArray(a, 4);
+	secondLargest(a, 4);
 } 
