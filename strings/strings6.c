238dd0e75b7402a198ebd171ecd8246f308adfbb
Pragati Gupta <praeast94@gmail.com>
2025-05-17 16:35:20 +0530

    function removeChar() running fine after using ! sign in place of= sign in strings6.c

--- strings/strings6.c
+++ strings/strings6.c
@@ -14,7 +14,7 @@ int largestOfTwo(int x, int y){
     else{
         return x;
     }
-}   
+}  
 
 void addTwoNumericalCharStrings(char s[], char r[]){
     int x = sizeOfString(s);
@@ -61,6 +61,76 @@ void addTwoNumericalCharStrings(char s[], char r[]){
         printf("%c", add[i]);
     }
 }
+
+void removeChar(char s[]){
+    printf("%c", s[0]);
+    for(int i = 1; i < sizeOfString(s); i++){
+        for(int j = 0; j < i; j++){
+            if(s[i] != s[j]){
+                if(j == i-1){
+                    printf("%c",s[i]);
+                }
+            }    
+        }
+    }
+}
+
+void mostFrequent(char s[]){
+    char k = s[0];
+    int counter[sizeOfString(s)];
+    int j = counter[0];
+    for(int i = 1; i < sizeOfString(s); i++){
+        counter[i] = 0;
+        for(int j = 0 ; j < i; i){
+            if(s[i] == s[j]);{
+                counter[i]++;
+            }
+        }
+    }
+    for(int i = 1; i < sizeOfString(s); i++){
+        if(j < counter[i]){
+            j = counter[i];
+            k = s[i];
+        }
+    }
+    printf("most frequent character in %s is = %c", s, k);
+}
+
+void solve(char s[]){
+    int d1 = 0;
+    int d2 = 0;
+    char a = '\0';
+    for(int i = 1; i < sizeOfString(s); i++){
+        if(s[i] == '+' || s[i] == '-'){
+            if(s[i] == '+'){
+                a = '+';
+            }
+            else{
+                a = '-';
+            }
+        }
+        else if( a == '\0'){
+            d1 = d1*10 + (s[i]-48);
+        }
+        else{
+            d2 = d2*10 + (s[i]-48);
+        }
+    }
+    if(a == '+'){
+        a = d1+d2;
+    }
+    else{
+        a = d1-d2;
+    }
+}
+
 void main(){
-    addTwoNumericalCharStrings("2354","353464");
+    // int arr[5] = {};
+    // for(int i = 0; i < 5; i++){
+    //     printf("%d\n", arr[i]);
+    // }
+    // addTwoNumericalCharStrings("2354","353464");
+    removeChar("hello");
+    // mostFrequent("122333444455555");
+    // solve("34+45");
 }
