2723d030babdc6bba84b6d92e04329a88c73e611
Pragati Gupta <praeast94@gmail.com>
2025-05-18 23:28:10 +0530

    function make of arrays1.c is running fine

--- Arrays/arrays1.c
+++ Arrays/arrays1.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+int sizeOfString(char str[]){
+    int counter = 0;
+    for(int i = 0; str[i] != '\0'; i++){
+        counter++;
+    }
+    return counter;
+}
+
 int countEvens(int l, int a[l]){
 	int counter = 0;
 	for(int i = 0; i < l; i++){
@@ -28,6 +36,30 @@ void count6and4(int a[], int l){
 	}
 }
 
+void stringOfSize(char a[], char b[], char c[], char d[], int n){
+	int x[] = {sizeOfString(a), sizeOfString(b), sizeOfString(c), sizeOfString(d)};
+	int counter = 0;
+	for(int i = 0; i < 4; i++){
+		if(x[i] == n){
+			counter++;
+		}
+	}
+	printf("%d size of string in string array is = %d", n, counter);
+}
+
+void make(int n){
+	printf("{");
+	for(int i = 0; i < n; i++){
+		if(i == n-1){
+			printf("%c%d%c", '"', i, '"');
+		}
+		else{
+			printf("%c%d%c,", '"', i, '"');
+		}
+	}
+	printf("}");
+}
+
 int compute(int a[], int x){
 	int l = a[0];
 	for( int i = 1; i < x; i++){
@@ -45,8 +77,12 @@ int compute(int a[], int x){
 }
 
 void main(){
-	int a[11]= {1,2,3,3,5,3,7, 8, 8, 8, 8};
-	printf("%d", countEvens(11, a));
-	count6and4(a, 11);
-	printf("%d", compute(a, 11));
+	// int a[11]= {1,2,3,3,5,3,7, 8, 8, 8, 8};
+	int n;
+	// printf("%d", countEvens(11, a));
+	// count6and4(a, 11);
+	// printf("%d", compute(a, 11));
+	// stringOfSize("A","DEF","BC","GHI",3);
+	scanf("%d", &n);
+	make(n);
 }
